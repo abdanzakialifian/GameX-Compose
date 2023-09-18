71a99402c8f9bafdea28385035032461fcd45a2c
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-09-18 08:58:42 +0700

    Ignore cpp class

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,6 +3,6 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_games_gamex_data_di_NetworkModule_gameApiKey(JNIEnv *env, jobject) {
-    std::string apiKey = "8a3a5fce65fd47008dbffaa2a0470c0b";
+    std::string apiKey = "";
     return env->NewStringUTF(apiKey.c_str());
 }
